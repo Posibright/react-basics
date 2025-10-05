3d9ed8e033dcaaa6d9a21258c595f25fd88aeca1
Posibright <posibright@gmail.com>
2025-10-05 23:03:51 +0100

    Initial Commit - React 2 SPA

---
+++ src/components/Contact.js
@@ -0,0 +1,4 @@
+function Contact() {
+  return <p>Contact us at posibright@gmail.com</p>;
+}
+export default Contact;
---
+++ src/components/Products.js
@@ -0,0 +1,4 @@
+function Products() {
+  return <p>Products and services</p>;
+}
+export default Products;
